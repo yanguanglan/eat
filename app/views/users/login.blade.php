623c7ea7a37303a4f16d3d42b1ff36cc791084a5
lan <lan@landeMacBook-Air.local>
2014-05-15 14:30:58 +0800

    init

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,7 +1,17 @@
 @extends('_layouts.mobile')
 @section('main')
+<script type="text/javascript">
+		$(function(){
+			var winHeight=$(window).height();
+			$(".login").height(winHeight);
+			//当改变窗口大小
+			$(window).resize(function(){
+				location.reload(); 
+			});
+		})
+	</script>
+{{ Form::open() }}
 <div class="login">
-	{{ Form::open() }}
 		<div class="loginCont">
 			<p>考勤订餐系统</p>
 			<h1>登录</h1>
@@ -12,6 +22,6 @@
 			<input type="password" name="password" placeholder="请输入密码"  class="input01"/>
 			<input type="submit" value="立即登录"  class="btn"/>
 		</div>
-		{{ Form::close() }}
 </div>
+{{ Form::close() }}
 @stop
